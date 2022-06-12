834f437936e58be538590a20c9652b57a3b8902d
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-12 18:50:23 +0800

    update solid latest and several server packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,27 @@
-import { render } from "solid-js/web";
+import type { Component } from 'solid-js';
 
-import "./index.scss";
+import logo from './logo.svg';
+import styles from './App.module.css';
 
-const App = () => (
-  <div class="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: client</div>
-    <div>Framework: solid-js</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
-render(App, document.getElementById("app"));
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
