3b8040438e280c28adccf02bf2422278900f0ee5
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-13 23:30:24 +0800

    chat screen wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,38 @@
-import type { Component } from 'solid-js';
+import { Component, createResource, For } from "solid-js";
+import { createClient } from "urql/core";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const client = createClient({
+  url: "http://127.0.0.1:4000/graphql",
+});
 
 const App: Component = () => {
+  const [todos] = createResource<
+    | void
+    | undefined
+    | readonly { id: string; text: string; done: boolean }[]
+    | null
+  >(() => {
+    client
+      .query(
+        `
+      query {
+        getTodos {
+          id
+          done
+          text
+        }
+      }
+  `
+      )
+      .toPromise()
+      .then(({ data }) => data.getTodos);
+  });
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
     </div>
   );
 };
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,38 @@
 import { createServer } from "@graphql-yoga/node";
 
+let todos = [
+  {
+    id: "1",
+    text: "Hello world",
+    done: false,
+  },
+];
+
+const typeDefs = `
+    type Todo {
+        id: ID!
+        done: Boolean!
+        text: String!
+    }
+    type Query {
+        getTodos: [Todo]!
+    }
+`;
+
+const resolvers = {
+  Query: {
+    getTodos: () => {
+      return todos;
+    },
+  },
+};
+
 // Create your server
-const server = createServer({});
+const server = createServer({
+  schema: {
+    typeDefs,
+    resolvers,
+  },
+});
 // start the server and explore http://localhost:4000/graphql
 server.start();
